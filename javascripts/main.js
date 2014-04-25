d3b126d24302d08abc25ede79141651a5a272494
Elliot Stokes <elliot.stokes@gmail.com>
2014-04-25 09:30:13 +0100

    updates to app add file dialog. Show image

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,22 @@
-require([], function() {
-	alert("loaded");
-});
+(function($) {
+
+	function chooseFile(name, callback) {
+		var chooser = $(name);
+		chooser.change(function(evt) {
+			callback($(this).val());
+		});
+
+		chooser.trigger('click');
+	}
+
+
+
+	$('#btn-load').click(function() {
+		chooseFile('#fileDialog', function(file) {
+			if (file) {
+				$('body').append($('<img/>').attr('src', file));
+			}
+		});
+	});
+
+})(jQuery);
