72a4bb6bbde927b8822f36c5a4ead8fd8483c057
Elliot Stokes <elliot.stokes@gmail.com>
2014-05-19 22:14:20 +0100

    move render to a canvas

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,33 @@
 (function($) {
 
+	//add a tray
+	var gui = require('nw.gui');
+
+	/*var tray = new gui.Tray({
+		title: 'Tray',
+		icon: 'img/icon.png'
+	});
+
+	var menu = new gui.Menu();
+	menu.append(new gui.MenuItem({
+		type: 'checkbox',
+		label: 'box1'
+	}));
+	tray.menu = menu;
+
+	*/
+	var menubar = new gui.Menu({
+		type: 'menubar'
+	});
+	menubar.append(new gui.MenuItem({
+		label: 'Item A'
+	}));
+
+	gui.Window.get().menu = menubar;
+
+	var canvasElement = $('.image-container').get(0),
+			context = canvasElement.getContext('2d');
+
 	function chooseFile(name, callback) {
 		var chooser = $(name);
 		chooser.change(function(evt) {
@@ -14,7 +42,15 @@
 	$('#btn-load').click(function() {
 		chooseFile('#fileDialog', function(file) {
 			if (file) {
-				$('body').append($('<img/>').attr('src', file));
+				var image = new Image();
+				image.onload = function() {
+					alert("loaded");
+					context.drawImage(image, 0, 0);
+				}
+				image.src = file;
+
+
+				//$('.image-container').get
 			}
 		});
 	});
